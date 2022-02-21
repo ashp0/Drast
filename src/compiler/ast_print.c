af7f6675d43afae754b30002b18945b009f6a341
Ashwin Paudel <62433766+Ashwin-Paudel@users.noreply.github.com>
2022-02-21 07:50:41 -0500

    AST Print: Fixed the function argument printing error

--- src/compiler/ast_print.c
+++ src/compiler/ast_print.c
@@ -11,6 +11,12 @@ static inline void ast_print_import(AST *ast);
 
 static inline void ast_print_variable(AST *ast);
 
+static inline void ast_print_function_declaration(AST *ast);
+
+static inline void ast_print_function_arguments(AST *ast);
+
+static inline void ast_print_type_name(AST *ast);
+
 void ast_print(AST *ast) {
     switch (ast->type) {
         case AST_TYPE_IMPORT:
@@ -20,8 +26,12 @@ void ast_print(AST *ast) {
         case AST_TYPE_LET_DEFINITION:
             ast_print_variable(ast);
             break;
+        case AST_TYPE_FUNCTION_DECLARATION:
+            ast_print_function_declaration(ast);
+            break;
         default:
-            printf("Cannot Print AST\n");
+            printf("Cannot Print AST %d\n", ast->type);
+            break;
     }
 }
 
@@ -41,5 +51,36 @@ static inline void ast_print_variable(AST *ast) {
             printf("(var/let) %s: %s\n", ast->value.Variable.identifier,
                    token_print(ast->value.Variable.value->value.ValueKeyword.token->type));
         }
+    }
+}
+
+static inline void ast_print_function_declaration(AST *ast) {
+    printf("func %s(", ast->value.FunctionDeclaration.function_name);
+    ast_print_function_arguments(ast);
+    printf(" -> ");
+    printf("%s {\n", token_print(ast->value.FunctionDeclaration.return_type->value.ValueKeyword.token->type));
+
+    for (int i = 0; i < ast->value.FunctionDeclaration.body_size; i++) {
+        printf("\t");
+        ast_print(ast->value.FunctionDeclaration.body[0]);
+    }
+
+    printf("}\n");
+}
+
+static inline void ast_print_function_arguments(AST *ast) {
+    for (int i = 0; i < ast->value.FunctionDeclaration.argument_size; i++) {
+        ast_print_type_name(ast->value.FunctionDeclaration.arguments[i]->value.FunctionArgument.argument_type);
+        printf(" %s, ", ast->value.FunctionDeclaration.arguments[i]->value.FunctionArgument.argument_name);
+    }
+
+    printf("\b\b)");
+}
+
+static inline void ast_print_type_name(AST *ast) {
+    if (ast->value.ValueKeyword.is_array) {
+        printf("%s[]", token_print(ast->value.ValueKeyword.token->type));
+    } else {
+        printf("%s", token_print(ast->value.ValueKeyword.token->type));
     }
 }
